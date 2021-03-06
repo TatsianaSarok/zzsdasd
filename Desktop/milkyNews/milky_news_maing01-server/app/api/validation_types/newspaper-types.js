467579c8cd990b77000dc4aa7b8ed8a3cf72460c
Tatsiana Sarokina <tatsiana_sarokina@plus4u.net>
2021-03-06 19:39:24 +0100

    delete mess

--- Desktop/milkyNews/milky_news_maing01-server/app/api/validation_types/newspaper-types.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable */
-const newspaperUpdateInitDtoInType = shape({
-    name: uu5String(200).isRequired(),
-   });
--- Desktop/milkyNews/milky_news_maing01-server/app/api/validation_types/topic-types.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable */
-const toicDeleteDtoInType = {
-    name: uu5String().isRequired()
-   };
